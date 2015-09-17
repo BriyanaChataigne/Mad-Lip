7d50943a91bbd1482d4cd2521b061098b6e3da9b
BriyanaChataigne <briyana.chataigne@techhigh.us>
2015-09-17 08:02:30 -0400

    Create lip.java
    Created lip.java

---
+++ lip.java
@@ -0,0 +1,133 @@
+import java.util.Scanner;
+public class lip {
+	public static void main(String args[]){
+		Scanner scnr = new Scanner(System.in);
+
+		String noun1 = "";
+		String adj1 = "";
+		String noun2 = "";
+		String noun3 = "";
+		String adj2 = "";
+		String adj3 = "";
+		String noun4 = "";
+		String noun5 = "";
+		String adj6 = "";
+		String adj7 = "";
+		String noun6 = "";
+		String noun7 = "";
+		String noun8 = "";
+		String noun9 = "";
+		String noun10 = "";
+		
+		System.out.println("Enter a noun:");
+		noun1 = scnr.nextLine();
+		
+		System.out.println("Enter a adjective:");
+		adj1 = scnr.nextLine();
+		
+		System.out.println("Enter a noun:");
+		noun2 = scnr.nextLine();
+		
+		System.out.println("Enter a noun:");
+		noun3 = scnr.nextLine();
+		
+		System.out.println("Enter a adjective:");
+		adj2 = scnr.nextLine();
+		
+		System.out.println("Enter a adjective:");
+		adj3 = scnr.nextLine();
+		
+		System.out.println("Enter a noun:");
+		noun4 = scnr.nextLine();
+		
+		System.out.println("Enter a noun:");
+		noun5 = scnr.nextLine();
+		
+		System.out.println("Enter a adjective:");
+		adj6 = scnr.nextLine();
+		
+		System.out.println("Enter a adjective:");
+		adj7 = scnr.nextLine();
+		
+		System.out.println("Enter a noun:");
+		noun6 = scnr.nextLine();
+		
+		System.out.println("Enter a noun");
+		noun7 = scnr.nextLine();
+		
+		System.out.println("Enter a noun:");
+		noun8 = scnr.nextLine();
+		
+		System.out.println("Enter a noun:");
+		noun9 = scnr.nextLine();
+		
+		System.out.println("Enter a noun:");
+		noun10 = scnr.nextLine();
+		
+		System.out.print("If you're ");
+		System.out.print(noun1);
+		
+		System.out.print(" and you're on the ");
+		System.out.print( adj1);
+
+		System.out.print(" you can count on the four of us taking you ");
+		System.out.println(noun2);
+
+		System.out.print(" Cause we're ");
+		System.out.print(noun3);
+
+		System.out.print(" and evil never ");
+		System.out.print(adj2);
+
+		System.out.println(" us ");
+	
+
+		System.out.print("We'll ");
+		System.out.print(adj3);
+
+		System.out.print(" the day and then go out for ");
+		System.out.println(noun4);
+
+		System.out.print("We are the ");
+		System.out.print(noun5);
+
+		System.out.print(" Gems. ");
+		
+
+		System.out.print("We'll always ");
+		System.out.print(adj6);
+
+		System.out.println(" the day ");
+
+		System.out.println("And if you think we can't,");
+
+		System.out.print(" We'll always ");
+		System.out.print(adj7);
+
+		System.out.println(" a way. ");
+			
+
+		System.out.print("That's why the ");
+			System.out.print(noun6);
+
+		System.out.print(" of this ");
+			System.out.print(noun7);
+
+		System.out.println(" believe in ");
+		
+		
+			System.out.println(noun8);
+
+			System.out.println(noun9);
+
+		System.out.println("And " + noun10);
+			
+
+		System.out.println("And Steven!");
+
+	
+			
+		
+	
+	}
+}
